1f0ec84343f803b252e81069d00bcb92f9059522
Selena Turner <snturner@ncsu.edu>
2018-03-12 18:16:33 -0400

    War is functional, may need some error tweaks

--- project/PlayWar.py
+++ project/PlayWar.py
@@ -28,36 +28,39 @@ roundNum = 0
 def giveCards(cardP1, cardP2, p1, p2):
     global cardList
     global tieList
-    global roundNum
-    roundNum = roundNum + 1
-    print('---------------------ROUND %d---------------------' % roundNum)
+
     print('Player 1 plays card ' + cardP1.string())
     print('Player 2 plays card ' + cardP2.string())
+    try:
+        p1.hand = p1.hand[1:]
+        p2.hand = p2.hand[1:]
 
-    p1.hand = p1.hand[1:]
-    p2.hand = p2.hand[1:]
-
-    if cardP1.value > cardP2.value:
-        cardList.append(cardP1)
-        cardList.append(cardP2)
-        print('Player 1 wins the round!')
-        print('-----------------------------------------------------')
-        return 1
-    elif cardP1.value < cardP2.value:
-        cardList.append(cardP2)
-        cardList.append(cardP1)
-        print('Player 2 wins the round!')
-        print('-----------------------------------------------------')
-        return 2
-    else:
-        cardList.append(p1.hand[0])
-        cardList.append(p1.hand[1])
-        cardList.append(p2.hand[0])
-        cardList.append(p2.hand[1])
-        p1.hand = p1.hand[2:]
-        p2.hand = p2.hand[2:]
-        winner = giveCards(p1.hand[0], p2.hand[0], p1, p2)
-        return winner
+        if cardP1.value > cardP2.value:
+            cardList.append(cardP1)
+            cardList.append(cardP2)
+            print('Player 1 wins the round!')
+            print('-----------------------------------------------------')
+            return 1
+        elif cardP1.value < cardP2.value:
+            cardList.append(cardP2)
+            cardList.append(cardP1)
+            print('Player 2 wins the round!')
+            print('-----------------------------------------------------')
+            return 2
+        else:
+            cardList.append(p1.hand[0])
+            cardList.append(p1.hand[1])
+            cardList.append(p2.hand[0])
+            cardList.append(p2.hand[1])
+            p1.hand = p1.hand[2:]
+            p2.hand = p2.hand[2:]
+            winner = giveCards(p1.hand[0], p2.hand[0], p1, p2)
+            return winner
+    except IndexError:
+        if len(p1.hand) == 0:
+            return 2
+        elif len(p2.hand) == 0:
+            return 1
 
 def main():
         
@@ -76,18 +79,21 @@ def main():
     p2 = PlayerWar()
     d = Dealer()
     global cardList
+    global roundNum
 
     d.dealWar(p1, p2)
 
     hasCards = True
     while hasCards:
+        roundNum = roundNum + 1
+        print('---------------------ROUND %d---------------------' % roundNum)
         winner = giveCards(p1.hand[0], p2.hand[0], p1, p2)
         if winner == 1:
             for c in cardList:
-                p1.hand.append(c)
+                p1.hand.append(cardList.pop())
         elif winner == 2:
             for c in cardList:
-                p2.hand.append(c)
+                p2.hand.append(cardList.pop())
         hasCards = (len(p1.hand) != 0) and (len(p2.hand) != 0)
         
     if not p1.hand:
