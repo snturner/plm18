bb3905cbf5a2b9f5be41f392b66a7716fd2a4fc5
Allen <athoma10@ncsu.edu>
2018-03-12 13:00:52 -0400

    added new file

--- project/Deck.py
+++ project/Deck.py
@@ -5,7 +5,7 @@ class Deck:
     bool up = True
     bool down = False
 
-    def __init__(self, decklist):
+    def __init__(self):
         makeList()
         random.shuffle(decklist)
     
@@ -13,6 +13,6 @@ class Deck:
         for x in range(0,4):
             for y in range(0, 13):
                 ##y is the value, x is suit
-                ## for y, 1 - A, 10 - 14 is J-K respectively 
+                ## for y, 10 - 14 is J-A respectively 
                 ## for x, 1 - SP, 2 - C, 3 - H, 4 - D
-                decklist[y + x] = Card(y + 1, x + 1, self.down, True)
+                decklist[y + x] = Card(y + 2, x + 1, self.down, True)
