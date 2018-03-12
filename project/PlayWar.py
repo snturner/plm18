244b247ca28a73720af7e67d1894f726a639b5b3
cmthomp7 <cmthomp7@ncsu.edu>
2018-03-12 13:09:27 -0400

    playWar file

--- project/PlayWar.py
+++ project/PlayWar.py
@@ -1,1 +1,18 @@
 # play war
+import Card
+import Deck
+import PlayerWar
+import 
+def main():
+  '''Starts game and shows welcome message and rules'''
+  print 'Welcome to WAR! \nEach player starts with half of a shuffled 52 card deck.
+  print 'Players may not look at the cards in their hands.' 
+  print 'Each player pulls one card from the deck and puts it on the table.' 
+  print 'The higher card wins; the player who played it takes both cards and places them on the bottom of his stack. 
+  print 'Aces are always higher than Kings in this game, but they are often lower than Jokers (if used).'
+  print 'A "war" occurs when the two players play the same rank (two 9s, for example), making a tie.' 
+  print 'Each player then deals three cards facedown (the spoils are for the winner) and a face-up card.' 
+  print 'The player with the higher face-up card takes all the cards from the war into his stack.'
+  
+  def launch_game
+  
