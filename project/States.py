3442e814ec3cd46126c3ff869178c738d7685a63
Selena Turner <snturner@ncsu.edu>
2018-04-07 16:39:24 -0400

    Added empty state machine file

---
+++ project/States.py
@@ -0,0 +1,23 @@
+class PickGame():
+    #empty for now
+
+class BeginGame():
+    #empty for now
+
+class Setup():
+    #empty for now
+
+class RoundBegin():
+    #empty for now
+
+class PlayerTurn():
+    #empty for now
+
+class RoundEnd():
+    #empty for now
+
+class Win():
+    #empty for now
+
+class EndGame():
+    #empy for now
