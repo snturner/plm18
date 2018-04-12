3d4f757c64816f7e81dedb2f697b0c5e72c6b1ee
Allen <athoma10@ncsu.edu>
2018-04-12 15:26:58 -0400

    Added an fsm

---
+++ project/FSM.py
@@ -0,0 +1,54 @@
+class Transition():
+    def __init__(self, guard, end):
+        self.guard = guard
+        self.end   = end
+        self.actions = []
+    
+    def addactions(self, f):
+        self.actions += f
+
+class State():
+    def __init__(self, onEntry=None, name):
+        if onEntry is not None: onEntry()
+        self.name = name
+        self.transitions = []
+
+    def addtransition(self, transition):
+         self.transitions += transition
+
+    def step(self):
+        for t in self.transitions:
+            if t.guards is not None:
+                if(t.guard is True):
+                    for action in t.actions:
+                        action()
+                    return t.end
+
+    def onEntry(self): pass
+
+    def onExit(self): pass
+
+class FSM():  
+    def __init__(self, start):
+        self.states = []
+        self.start = start
+        self.currentstate = start
+
+    def addState(self, state):
+        self.states += state
+
+    def run(self):
+        while True:
+            self.currentstate = currentstate.step()
+            if state.quit():
+                break
+
+class Bartok():
+     def __init__(self):
+        run()
+    def run():
+    fsm = FSM()
+    start = Start(startGame())
+    startGame = State
+
+
