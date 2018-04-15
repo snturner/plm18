bb779000f1220b9ca48fe34f4444941e1bec9b6d
Allen <athoma10@ncsu.edu>
2018-04-15 16:22:44 -0400

    Added more changes to the War FSM Machine

--- project/FSM.py
+++ project/FSM.py
@@ -1,3 +1,6 @@
+from War import *
+from Player import Player
+from Dealer import Dealer
 class Transition():
     def __init__(self, guard, end):
         self.guard = guard
@@ -5,91 +8,91 @@ class Transition():
         self.actions = []
     
     def addactions(self, f):
-        self.actions += f
+        self.actions.append(f)
 
 class State():
-    def __init__(self, onEntry=None, name):
+    def __init__(self, name, gameResources, onEntry=None):
         if onEntry is not None: onEntry()
+        self.gameResources = gameResources
         self.name = name
         self.transitions = []
 
     def addtransition(self, transition):
-         self.transitions += transition
+         self.transitions.append(transition)
+         #transition.actions[0]()
 
     def step(self):
         for t in self.transitions:
-            if t.guards is not None:
+            if t.guard is not None:
                 if(t.guard is True):
                     for action in t.actions:
-                        action()
+                        action(gameResources)
                     return t.end
 
     def onEntry(self): pass
 
-    def onExit(self): pass
+
+class Start(State): pass
+
+class End(State):
+    def quit(self):
+        return True
 
 class FSM():  
     def __init__(self, start):
         self.states = []
         self.start = start
         self.currentstate = start
+        self.run()
 
     def addState(self, state):
         self.states += state
 
     def run(self):
         while True:
-            self.currentstate = currentstate.step()
-            if state.quit():
+            self.currentstate = self.currentstate.step()
+            if isinstance(self.currentstate, End):
                 break
 
-class Bartok():
-     def __init__(self):
-        run()
-    def run():
-        #states
-        start = State(startGame(), "start")
-        roundBegin = 
-        playerTurn = 
-        roundEnd = 
-        win =
-        endGame =
-        
-        #transitions
-        setupT = Transition(true, startGame)
-        setUpT.addactions(setupGame)
-        start.addtransition(setupT)
-
-        #setup the fsm
-        fsm = FSM(start)
-
-        
-class War():
+class WarM():
     def __init__(self):
-        run()
-    def run(): 
+        self.gameResources = {}
+        self.gameResources["players"] = []
+        self.run()
+
+    def run(self): 
         #states
-        start = State(startGame(), "start") #does there have to be a different startGame for war and bartok?
-        roundBegin = 
-        playerTurn = 
-        roundEnd = 
-        win =
-        endGame =
+        start = Start("start", self.gameResources)
+        end = End("end", self.gameResources)
+        #roundBegin = State(None, "begin round")
+        #playerTurn = 
+        #roundEnd = 
+        #win =
+        #endGame =
         
         #transitions
-        setupT = Transition(true, startGame)
-        setUpT.addactions(setupGame)
+        #start transitions
+        setupT = Transition(True, end)
+        setupT.addactions(setUp)
         start.addtransition(setupT)
 
         #setup the fsm
         fsm = FSM(start)
+
+    #creates players and deals cards to the players
+    def setUp(gameResources):
+        print("h1")
+        gameResources["players"].append(Player())
+        gameResources["players"].append(Player())
+        d = Dealer()
+        print("h2")
         
-#Based on https://github.com/shryock/csc495-project/blob/master/src/games/CrazyEightsFSM.py  
-def __main__():
-    
+
+if  __name__ =='__main__':
+    war = WarM()
     try:
-        game = FSM()
+        war = WarM()
     except:
         print("\n\nGame Ended unexpectedly.\n")
-        Logger.close()
+        #Logger.close()
         exit()
