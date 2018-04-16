decc8169c08215e323e45fa6be9caa33765e3842
Selena Turner <snturner@ncsu.edu>
2018-04-16 10:54:23 -0400

    Swapped Bartok rules to BartokRules.py file, not completely done yet

---
+++ project/BartokRules.py
@@ -0,0 +1,83 @@
+from Player import Player
+from Dealer import Dealer
+from Card import Card
+def printInstruction(gameResources):
+    '''Starts game and shows welcome message and rules'''
+    print("Rules of Bartok:\n")
+    print("There must be at least 2 players and at most 6 players.")
+    print("Players play turn-based rounds where one player")
+    print("plays a card from their hand only if that card matches either the")
+    print("value or the suit of the faced-up card and draws a card from ")
+    print("the deck if the player has no matches. Then the next player has a turn.")
+    print("The game is won by the player who has no cards left in their hand.\n")
+
+#creates players and deals cards to the players
+def setUp(gameResources):
+    gameResources["players"] = []
+    while loop:
+        try:
+            print("Enter number of players (2 - 6):")
+            numOfPlayers = int(input())
+            if numOfPlayers < 2 or numOfPlayers >= 7:
+                print("There must be between 2 - 6 players")
+            else:
+                loop = False
+        except:
+            print("Enter a valid number")
+    gameResources["numPlayers"] = numOfPlayers
+    for i in range(0, numOfPlayers):
+        gameResources["players"].append(Player())
+    gameResources["currentPlayer"] = gameResources["players"][0]
+    d = Dealer()
+    d.dealHand(gameResources["players"], numOfPlayers, 6)
+    gameResources["discardList"] = []
+    gameResources["discardList"].append(d.dealCard())
+
+# prints out the next player's start of turn
+def nextPlayer(gameResources, currPlayerNum)
+    gameResources["currentPlayer"] = gameResources["players"][currPlayerNum]
+    print("---------------------PLAYER %d's TURN---------------------" % gameResources["playerNum"])
+    print("Current card on top: %s" % Card.string(gameResources["discardList"][0]))
+    k = 1
+    for card in gameResources["currentPlayer"].hand:
+        cardStr = Card.string(card)
+        print(" %(num)d) %(card)s" % {"num": k, "card":cardStr})
+        k += 1
+
+# player selects card through command line
+def playerPicksCard(gameResources):
+    player = gameResources["currentPlayer"]
+    print("Enter desired card position to play (enter -1 to draw):")
+    cardnum = int(input())
+    if cardnum == -1:
+        print("Player %(num)d draws a card"% {"num": (i + 1)})
+        player.hand.append(d.dealCard())
+    else:
+        gameResources["cardpicked"] = player.hand[(cardnum - 1)]
+        
+# player card is played
+def playCard(gameResources):
+    player = gameResources["currentPlayer"]
+    cardToPlay = gameResources["cardpicked"]
+    if Card.compare(cardToPlay.value, gameResources["discardList"][0].value) == 0 or Card.compare(cardToPlay.suit, gameResources["discardList"][0].suit) == 0:
+        gameResources["discardList"].insert(0, cardPlayed)
+        del player.hand[(cardnum - 1)]
+    else:
+        print("Invalid card, Player %(num)d draws a card"% {"num": (i + 1)})
+        player.hand.append(d.dealCard())
+
+# winner is printed
+def winner(gameResources):
+    players = gameResources["players"]
+    numPlayers = gameResources["numPlayers"]
+    for i in range(0, numPlayers)
+        if(len(d.deck.cards) <= 0):
+            print("The game is a tie!")
+            break
+        if(len(players[i].hand) <= 0):
+            print("Player %d wins!" %  (i + 1))
+            break
+        
+    
+    
+    
